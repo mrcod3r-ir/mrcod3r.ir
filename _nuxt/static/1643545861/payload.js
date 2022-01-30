6e1cc42a8e9136a6517f9a17933d324045fcfd18
mr cod3r <mrcod3r.ir@gmail.com>
2022-01-30 16:11:01 +0330

    nuxt content blog test

---
+++ _nuxt/static/1643545861/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", {data:[{articles:[{slug:"first-post",description:"Welcome to my first blog post using content module",title:"My first blog post",img:"blog-1.jpg"},{slug:"second-post",description:"Welcome to my second blog post using content module",title:"My second blog post",img:"blog-2.jpg"}]}],fetch:{},mutations:void 0});
