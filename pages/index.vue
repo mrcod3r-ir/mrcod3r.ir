14283af4741d1fabf0789c6f08dcdbff6a10361f
mr cod3r <mrcod3r.ir@gmail.com>
2022-08-15 13:43:10 +0430

    init

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ data.title }}</h1>
+    <!-- <ContentDoc :value="data" /> -->
+  </div>
+</template>
+
+<script setup>
+const { data } = await useAsyncData("", () => queryContent("/").findOne());
+
+definePageMeta({
+  title: "Some Page",
+});
+</script>
+
+<style scoped></style>
